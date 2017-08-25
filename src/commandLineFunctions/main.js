973c7443bc99f16de539bf94ac72f54bba34a605
Oceane Patiny <oceane@patiny.com>
2017-08-25 18:57:17 +0000

    add goToAngle.js

--- src/commandLineFunctions/main.js
+++ src/commandLineFunctions/main.js
@@ -1,3 +1,4 @@
+'use strict'
 /* This document will require all the different functions that we developed, like making to rotations,
  going to a specific point, moving three meters forward,... It uses command line arguments
  (https://www.youtube.com/watch?v=yTJ9OJmbiHU).
@@ -24,6 +25,7 @@ var goToPoint = require('./goToPoint.js');
 var movingOnCircle = require('./movingOnCircle.js');
 var moveDistance = require('./moveDistance.js');
 var spiral = require('./spiral.js');
+var goToAngle = require('./goToAngle.js');
 
 debug('functions required');
 
@@ -35,7 +37,6 @@ board.on('ready', async function () {
         return (index === -1) ? undefined : process.argv[index + 1];
     }
 
-
     // parameters that can vary (here, they come from the process.argv)
     const radiusCenter = grab('--r'); // radius of the circle the mass runs on in [mm]
     const numberRotation = grab('--tour'); // number of rotations we want the mass to do (whole numbers), if it is unlimited, write Infinity
@@ -44,10 +45,11 @@ board.on('ready', async function () {
     const length = grab('--l'); // distance the cylinder has to move in [mm]
     const timeToWait = grab('--d'); // delay to wait [ms]
     const numberSpires = grab('--spires'); // number of time you want the mass to go around on your spiral
+    const angleCenter = grab('--a'); // value of the angle of the mass in degrees
 
-    debug(radiusCenter, numberRotation, xMassPosition, yMassPosition, length, timeToWait);
+    debug(radiusCenter, numberRotation, xMassPosition, yMassPosition, length, timeToWait, angleCenter);
 
-    if (!(radiusCenter) && !(xMassPosition && yMassPosition) && !length && !timeToWait && !numberSpires) {
+    if (!(radiusCenter && numberRotation) && !(xMassPosition && yMassPosition) && !length && !timeToWait && !numberSpires && !(angleCenter && radiusCenter)) {
         console.log('No data to execute.');
     }
 
@@ -57,7 +59,7 @@ board.on('ready', async function () {
         console.log(`You waited ${timeToWait} [ms]`);
     }
 
-    if (radiusCenter) {
+    if (radiusCenter && numberRotation) {
         console.log('You are in movingOnCircle');
         await movingOnCircle(radiusCenter, numberRotation);
     }
@@ -73,6 +75,11 @@ board.on('ready', async function () {
     if (numberSpires) {
         console.log('You are in spiral');
         await spiral(numberSpires);
+    }
+
+    if (angleCenter && radiusCenter) {
+        console.log('You are in goToAngle');
+        await goToAngle(angleCenter, radiusCenter);
     }
 
 });
